722112d6d2ce9e596350bc8b9eb15115fa4466b5
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-25 00:22:22 -0800

    Fix into_boxed_slice() being annoying

--- src/main/rust/src/chunk.rs
+++ src/main/rust/src/chunk.rs
@@ -10,7 +10,22 @@ fn pos_to_index(pos: ChunkRelPos) -> usize {
 }
 
 impl Chunk {
-  pub fn new() -> Chunk { Chunk { data: vec![0; 65536].into_boxed_slice() } }
+  pub fn new() -> Chunk {
+    // vec![0; 65536].into_boxed_slice() does the same thing, but it builds the
+    // whole thing on the stack first in debug mode.
+    unsafe {
+      use std::alloc::{alloc_zeroed, Layout};
+
+      let layout = Layout::array::<u16>(65536).unwrap();
+      // Pointer casts from a *mut u8 to a *mut u16. This is safe because the layout
+      // is u16 aligned.
+      let ptr = alloc_zeroed(layout).cast();
+      // Builds a raw slice from the given pointer, which was just allocated and
+      // aligned correctly.
+      let slice_ptr = core::ptr::slice_from_raw_parts_mut(ptr, 65536);
+      Chunk { data: Box::from_raw(slice_ptr) }
+    }
+  }
 
   pub fn set(&mut self, pos: ChunkRelPos, block_id: u16, block_data: u8) {
     assert!(block_id < 4096);
