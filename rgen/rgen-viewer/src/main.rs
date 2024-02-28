797eb3d5f64d6fd0201b3a534e825ba67353752f
Jaimie <jaimiegrichards@gmail.com>
2024-02-27 20:56:50 -0800

    broken but finished hillshading

--- rgen/rgen-viewer/src/main.rs
+++ rgen/rgen-viewer/src/main.rs
@@ -97,12 +97,37 @@ pub fn main() -> Result<(), String> {
             let height = world.height(pos);
             let meter_height = world.meter_height(pos);
 
-            let mut basecolor = 255;
-            if meter_height >= 64.0 {
-              basecolor = 255 - (((meter_height - 64.0) / 64.0) * 255.0) as u8;
-            }
+            let cross_bottom = world.meter_height(pos + Pos::new(0, 0, -1));
+            let cross_top = world.meter_height(pos + Pos::new(0, 0, 1));
+            let dz_dx = (cross_bottom - cross_top) / (2.0 * 1.0);
 
-            let greycolor = Color::RGB(basecolor, basecolor, basecolor);
+            let cross_right = world.meter_height(pos + Pos::new(1, 0, 0));
+            let cross_left = world.meter_height(pos + Pos::new(-1, 0, 0));
+            let dz_dy = (cross_right - cross_left) / (2.0 * 1.0);
+
+            //claculates cell slope at that location
+
+            let cell_slope = ((dz_dx).powi(2) + (dz_dy).powi(2)).sqrt().atan();
+            //dbg!(cell_slope);
+            //Slope = arctan(sqrt((dz/dx)^2 + (dz/dy)^2))
+
+            //calulates cell aspect this is the direction the cell is facing as a slope
+            let cell_aspect = dz_dx.atan2(-dz_dy);
+            //arctan2(dz/dx, -dz/dy)
+
+            let azimuth = 315.0 / 180.0 * std::f64::consts::PI;
+            let altidue = 45.0 / 180.0 * std::f64::consts::PI;
+
+            let solar_incidence_angle = (altidue.sin() * cell_slope.sin()
+              + altidue.cos() * cell_slope.cos() * (azimuth - cell_aspect).cos())
+            .acos();
+
+            let brightness = ((((solar_incidence_angle).cos() + 1.0) / 2.0) * 255.0) as u8;
+
+            let brightness = (meter_height * 2.0) as u8;
+            //let brightness = (cell_slope * 255.0) as u8;
+
+            let greycolor = Color::RGB(brightness, brightness, brightness);
 
             let color = match Biome::from_raw_id(biome_id.into()) {
               b if b == world.context.biomes.cold_taiga => 0xffff00,
