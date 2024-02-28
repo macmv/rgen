a841dc8c8d5d1a70b117d67ed2079fad8611160a
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-27 22:08:56 -0800

    Fix colors being invertted

--- rgen/rgen-viewer/src/render.rs
+++ rgen/rgen-viewer/src/render.rs
@@ -28,9 +28,9 @@ impl RenderBuffer {
 
     let index = (y * self.width as i32 + x) as usize * 4;
 
-    self.buffer[index + 0] = color.r;
+    self.buffer[index + 0] = color.b;
     self.buffer[index + 1] = color.g;
-    self.buffer[index + 2] = color.b;
+    self.buffer[index + 2] = color.r;
     self.buffer[index + 3] = color.a;
   }
 
