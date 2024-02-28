1e3bb46a9772f2cd7524a8f3692535efcbbf6962
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-27 20:29:13 -0800

    Add items for blocks

--- src/main/java/net/macmv/rgen/RGen.java
+++ src/main/java/net/macmv/rgen/RGen.java
@@ -1,18 +1,23 @@
 package net.macmv.rgen;
 
 import net.macmv.rgen.block.RBlocks;
+import net.macmv.rgen.item.RItems;
 import net.macmv.rgen.world.WorldTypeRGen;
 import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.registries.IForgeRegistry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = RGen.MODID, version = RGen.VERSION)
+@Mod.EventBusSubscriber(value = Side.CLIENT, modid = RGen.MODID)
 public class RGen {
   public static final String MODID = "rgen";
   public static final String VERSION = "1.0";
@@ -33,5 +38,17 @@ public class RGen {
     IForgeRegistry<Block> reg = event.getRegistry();
 
     RBlocks.registerBlocks(reg);
+  }
+
+  @SubscribeEvent
+  public void registerItems(RegistryEvent.Register<Item> event) {
+    IForgeRegistry<Item> reg = event.getRegistry();
+
+    RItems.registerItems(reg);
+  }
+
+  @SubscribeEvent
+  public static void registerModels(ModelRegistryEvent event) {
+    RItems.registerModels();
   }
 }
---
+++ src/main/java/net/macmv/rgen/item/RItems.java
@@ -0,0 +1,46 @@
+package net.macmv.rgen.item;
+
+import net.macmv.rgen.block.RBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.registries.IForgeRegistry;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RItems {
+  private static final Set<Item> items = new HashSet<>();
+
+  public static final Item THATCH_ROOF = registerBlockItem(RBlocks.THATCH_ROOF);
+  public static final Item DERP_DOG = registerBlockItem(RBlocks.DERP_DOG);
+
+  private static Item registerBlockItem(Block block) {
+    ResourceLocation path = block.getRegistryName();
+    if (path == null) {
+      throw new IllegalArgumentException("block must have a registry name");
+    }
+
+    Item item = new ItemBlock(block);
+    item.setRegistryName(path);
+    item.setUnlocalizedName(path.getResourcePath());
+    items.add(item);
+    return item;
+  }
+
+  public static void registerItems(IForgeRegistry<Item> reg) {
+    for (Item i : items) {
+      reg.register(i);
+    }
+  }
+
+  public static void registerModels() {
+    for (Item i : items) {
+      ModelResourceLocation location = new ModelResourceLocation(i.getRegistryName(), "inventory");
+      ModelLoader.setCustomModelResourceLocation(i, 0, location);
+    }
+  }
+}
