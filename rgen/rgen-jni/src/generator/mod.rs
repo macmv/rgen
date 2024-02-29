e23e618d9525cb030e2c52c25fa5d04c0e8cceaf
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-28 19:27:26 -0800

    Remove unused duplicate height maps

--- rgen/rgen-jni/src/generator/mod.rs
+++ rgen/rgen-jni/src/generator/mod.rs
@@ -1,12 +1,8 @@
-use crate::ChunkContext;
-use rgen_base::{Blocks, Chunk, ChunkPos, ChunkRelPos, Pos};
-use rgen_placer::noise::{NoiseGenerator, OctavedNoise, PerlinNoise};
+use rgen_base::{Blocks, Chunk, ChunkPos, Pos};
 use rgen_world::{Context, Generator, PartialWorld};
 
 pub struct TerrainGenerator {
   seed: u64,
-
-  height_map: OctavedNoise<PerlinNoise>,
 
   biomes: rgen_biome::WorldBiomes,
 }
@@ -31,27 +27,7 @@ impl Generator for TerrainGenerator {
 
 impl TerrainGenerator {
   pub fn new(blocks: &Blocks, biome_ids: &rgen_base::Biomes, seed: u64) -> TerrainGenerator {
-    TerrainGenerator {
-      seed,
-      height_map: OctavedNoise { octaves: 8, freq: 1.0 / 512.0, ..Default::default() },
-
-      biomes: rgen_biome::WorldBiomes::new(blocks, biome_ids),
-    }
-  }
-
-  pub fn generate(&self, ctx: &ChunkContext, chunk: &mut Chunk) {
-    for rel_x in 0..16_u8 {
-      for rel_z in 0..16_u8 {
-        let pos = ctx.chunk_pos.min_block_pos() + Pos::new(rel_x.into(), 0, rel_z.into());
-
-        let height =
-          ((self.height_map.generate(pos.x as f64, pos.z as f64, self.seed) + 1.0) * 64.0) as i32;
-
-        for y in 0..height as u8 {
-          chunk.set(ChunkRelPos::new(rel_x, y, rel_z), ctx.blocks.stone);
-        }
-      }
-    }
+    TerrainGenerator { seed, biomes: rgen_biome::WorldBiomes::new(blocks, biome_ids) }
   }
 }
 
@@ -61,15 +37,16 @@ mod tests {
 
   use super::*;
 
+  // FIXME: Rewrite this test.
   #[test]
   fn test_generator() {
-    let mut chunk = Chunk::new();
+    let _chunk = Chunk::new();
     let blocks = Blocks::test_blocks();
     let biomes = Biomes::test_blocks();
-    let generator = TerrainGenerator::new(&blocks, &biomes, 1);
+    let _generator = TerrainGenerator::new(&blocks, &biomes, 1);
 
-    let ctx = ChunkContext { chunk_pos: ChunkPos::new(0, 0), blocks: &blocks };
+    let _ctx = ChunkContext { chunk_pos: ChunkPos::new(0, 0), blocks: &blocks };
 
-    generator.generate(&ctx, &mut chunk);
+    // generator.generate(&ctx, &mut chunk);
   }
 }
