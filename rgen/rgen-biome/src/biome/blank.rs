28ac62adb38854d2604c88c225ab1bfa92a1ad2a
Neil Macneale V <neil.macneale.v@gmail.com>
2024-02-29 01:00:17 -0800

    Add in the valley table with temporary biomes

--- rgen/rgen-biome/src/biome/blank.rs
+++ rgen/rgen-biome/src/biome/blank.rs
@@ -4,3 +4,43 @@ pub fn blank(ctx: &IdContext, gen: &mut BiomeBuilder) {
   gen.id = ctx.biomes.plains;
   gen.top_block = ctx.blocks.grass;
 }
+
+pub fn rocky_valley(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn bog(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn cool_valley(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn warm_valley(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn swamp(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn dry_river(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn hot_swamp(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
+
+pub fn tropic_swamp(ctx: &IdContext, gen: &mut BiomeBuilder) {
+  gen.id = ctx.biomes.plains;
+  gen.top_block = ctx.blocks.grass;
+}
--- rgen/rgen-biome/src/table.rs
+++ rgen/rgen-biome/src/table.rs
@@ -6,25 +6,19 @@ type BiomeFnTable = &'static [&'static [BiomeFn]];
 // TODO: Need all of these biomes.
 /*
 const VALLEY_TABLE: [[&str; 6]; 7] = [
-  [rocky_valley, rocky_valley, bog, bog, bog, bog],
-  [rocky_valley, cool_valley, cool_valley, cool_valley, bog, bog],
-  [cool_valley, cool_valley, warm_valley, warm_valley, bog, swamp],
-  [dry_river, warm_valley, warm_valley, warm_valley, swamp, swamp],
-  [dry_river, swamp, warm_valley, swamp, swamp, hot_swamp],
-  [dry_river, dry_river, swamp, swamp, hot_swamp, hot_swamp],
-  [dry_river, dry_river, swamp, hot_swamp, hot_swamp, tropic_swamp],
 ];
 */
 
 const BLANK_TABLE: BiomeFnTable = &[&[blank]];
 
 const VALLEY_TABLE: BiomeFnTable = &[
-  &[plains, plains, plains, plains, plains, plains, plains, plains],
-  &[plains, plains, plains, plains, plains, plains, plains, plains],
-  &[plains, plains, plains, plains, plains, plains, plains, plains],
-  &[plains, plains, plains, plains, plains, plains, plains, plains],
-  &[plains, plains, plains, plains, plains, plains, plains, plains],
-  &[plains, plains, plains, plains, plains, plains, plains, plains],
+  &[rocky_valley, rocky_valley, bog, bog, bog, bog],
+  &[rocky_valley, cool_valley, cool_valley, cool_valley, bog, bog],
+  &[cool_valley, cool_valley, warm_valley, warm_valley, bog, swamp],
+  &[dry_river, warm_valley, warm_valley, warm_valley, swamp, swamp],
+  &[dry_river, swamp, warm_valley, swamp, swamp, hot_swamp],
+  &[dry_river, dry_river, swamp, swamp, hot_swamp, hot_swamp],
+  &[dry_river, dry_river, swamp, hot_swamp, hot_swamp, tropic_swamp],
 ];
 
 pub struct Tables {
