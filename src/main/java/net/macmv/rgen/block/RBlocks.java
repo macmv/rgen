46cfefd8ec02d7a2957ef544a0398a498d993e38
Jaimie <jaimiegrichards@gmail.com>
2024-03-02 00:25:34 -0800

    dead log (WIP)

--- src/main/java/net/macmv/rgen/block/RBlocks.java
+++ src/main/java/net/macmv/rgen/block/RBlocks.java
@@ -2,7 +2,13 @@ package net.macmv.rgen.block;
 
 import net.macmv.rgen.RGen;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockLog;
+import net.minecraft.block.BlockNewLog;
+import net.minecraft.block.BlockOldLog;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.registries.IForgeRegistry;
 
 import java.util.HashSet;
@@ -14,6 +20,9 @@ public final class RBlocks {
   // FIXME: Need block items.
   public static final Block THATCH_ROOF = register("thatch_roof", new ThatchRoofBlock(Material.ROCK));
   public static final Block DERP_DOG = register("derp_dog", new Block(Material.CLOTH));
+
+  public static final Block DEAD_LOG = register("dead_log", new BlockLog() {});
+
 
   private static Block register(String name, Block block) {
     Block b = block.setRegistryName(RGen.MODID, name).setUnlocalizedName(name);
