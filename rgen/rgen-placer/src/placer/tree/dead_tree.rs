34bf6fd77c6898cadc4b6bde404c2e42321194bb
Jaimie <jaimiegrichards@gmail.com>
2024-03-02 02:25:08 -0800

    updated deadtrees

--- rgen/rgen-placer/src/placer/tree/dead_tree.rs
+++ rgen/rgen-placer/src/placer/tree/dead_tree.rs
@@ -1,16 +1,16 @@
-use rgen_base::{Block, Pos};
+use rgen_base::{Block, BlockSet, BlockState, Pos};
 use rgen_world::PartialWorld;
 
 use crate::{Placer, Random, Rng};
 
 pub struct DeadTree {
-  pub trunk: Block,
+  pub trunk: BlockState,
 }
 
 impl Placer for DeadTree {
   fn radius(&self) -> u8 { 1 }
 
-  fn avg_per_chunk(&self) -> f64 { 16.0 }
+  fn avg_per_chunk(&self) -> f64 { 2.0 }
 
   fn place(&self, world: &mut PartialWorld, rng: &mut Rng, pos: Pos) {
     let height = rng.rand_inclusive(4, 7);
