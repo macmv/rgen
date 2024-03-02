0613078d82f18774d30eaa31d8f39ce625099aa8
Jaimie <jaimiegrichards@gmail.com>
2024-03-01 22:47:47 -0800

    Adds the scatter feature placer feature

--- rgen/rgen-placer/src/placer/mod.rs
+++ rgen/rgen-placer/src/placer/mod.rs
@@ -1,7 +1,9 @@
+mod scatter;
 mod splatter;
 mod splotch;
 mod tree;
 
+pub use scatter::Scatter;
 pub use splatter::Splatter;
 pub use splotch::Splotch;
 pub use tree::BasicTree;
---
+++ rgen/rgen-placer/src/placer/scatter.rs
@@ -0,0 +1,32 @@
+use rgen_base::{Block, BlockState, Pos};
+use rgen_world::PartialWorld;
+
+use crate::{rng::Random, Placer, Rng};
+
+pub struct Scatter {
+  pub replace: BlockState,
+  pub place:   BlockState,
+
+  pub attempts: u32,
+}
+
+impl Placer for Scatter {
+  fn radius(&self) -> u8 { 8 }
+
+  fn place(&self, world: &mut PartialWorld, rng: &mut Rng, pos: Pos) {
+    for _ in 0..self.attempts {
+      let pos = pos
+        + Pos::new(
+          rng.rand_inclusive(-8, 8),
+          rng.rand_inclusive(-4, 4) as u8,
+          rng.rand_inclusive(-8, 8),
+        );
+
+      let above_pos = pos + Pos::new(0, 1, 0);
+
+      if world.get(pos) == self.replace && world.get(above_pos).block != Block::AIR {
+        world.set(above_pos, self.place);
+      }
+    }
+  }
+}
