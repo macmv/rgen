2e94528887c2e7fed9c37e18969ad511f4fb5132
Neil Macneale V <neil.macneale.v@gmail.com>
2024-03-01 16:54:30 -0800

    Add splotch placer

--- rgen/rgen-placer/src/placer/mod.rs
+++ rgen/rgen-placer/src/placer/mod.rs
@@ -1,5 +1,7 @@
 mod splatter;
+mod splotch;
 mod tree;
 
 pub use splatter::Splatter;
+pub use splotch::Splotch;
 pub use tree::BasicTree;
---
+++ rgen/rgen-placer/src/placer/splotch.rs
@@ -0,0 +1,41 @@
+use rgen_base::{BlockState, Pos};
+use rgen_world::PartialWorld;
+
+use crate::{rng::Random, Placer, Rng};
+
+pub struct Splotch {
+  pub replace: BlockState,
+  pub place:   BlockState,
+
+  pub radius: u8,
+}
+
+impl Placer for Splotch {
+  fn radius(&self) -> u8 { self.radius }
+  fn avg_per_chunk(&self) -> f64 { 1.0 }
+
+  fn place(&self, world: &mut PartialWorld, rng: &mut Rng, pos: Pos) {
+    let r2 = (self.radius as i32).pow(2);
+
+    for x in -(self.radius as i32)..=self.radius as i32 {
+      for y in -(self.radius as i32)..=self.radius as i32 {
+        for z in -(self.radius as i32)..=self.radius as i32 {
+          let pos = pos + Pos::new(x, y as u8, z);
+
+          let dist2 = x.pow(2) + y.pow(2) + z.pow(2);
+          if dist2 > r2 {
+            continue;
+          }
+
+          if dist2 > rng.rand_inclusive(r2 / 4 * 3, r2) {
+            continue;
+          }
+
+          if world.get(pos) == self.replace {
+            world.set(pos, self.place);
+          }
+        }
+      }
+    }
+  }
+}
