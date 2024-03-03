5faf98937a37360fa77e079ec3c04bafe1c7a2d5
Neil Macneale V <neil.macneale.v@gmail.com>
2024-03-02 23:49:21 -0800

    Add MathUtil

---
+++ src/main/java/net/macmv/rgen/MathUtil.java
@@ -0,0 +1,13 @@
+package net.macmv.rgen;
+
+import net.minecraft.util.math.AxisAlignedBB;
+
+public class MathUtil {
+  public static AxisAlignedBB aabb(int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
+    if (minX < 0 || minX >= 16 || minY < 0 || minY >= 16 || minZ < 0 || minZ >= 16 || maxX < 0 || maxX >= 16 || maxY < 0 || maxY >= 16 || maxZ < 0 || maxZ >= 16) {
+      throw new IllegalArgumentException("all arguments must be within 0..16");
+    }
+
+    return new AxisAlignedBB(minX * 16.0, minY * 16.0, minZ * 16.0, maxX * 16.0, maxY * 16.0, maxZ * 16.0);
+  }
+}
