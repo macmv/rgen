c9147c9ea73bc6b4cf90217794bc0ccc03835c94
Jaimie <48069739+JaimieGR@users.noreply.github.com>
2024-03-04 12:13:33 -0800

    More mossy blocks (#10)
    * more mossy json & texture updates yes again, also a bunch of names have changed so be aware
    
    * adds mossy block java code and some mossy block clases
    
    * adds english (not like the English be it the tea, the people, the country of england or even the new 2.0 version where neelman lives) for moss blocks (the can't speak english but it gives them english names not to be confused with their mossosian-speak names)
    
    * some more notes on moss
    
    * adds workspace Now: with the basic texture for the incubator
    
    * mossy birch textrue for the workspace

---
+++ src/main/java/net/macmv/rgen/block/RGenPlantableStone.java
@@ -0,0 +1,35 @@
+package net.macmv.rgen.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockStone;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.EnumPlantType;
+import net.minecraftforge.common.IPlantable;
+
+public class RGenPlantableStone extends Block {
+
+    public RGenPlantableStone() {
+        super(Material.ROCK);
+        this.setCreativeTab(CreativeTabs.DECORATIONS);
+
+    }
+    public boolean canSustainPlant(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing direction, IPlantable plantable)
+    {
+        IBlockState plant = plantable.getPlant(world, pos.offset(direction));
+        net.minecraftforge.common.EnumPlantType plantType = plantable.getPlantType(world, pos.offset(direction));
+
+        if(plant.getBlock() == Blocks.SAPLING)
+            return true;
+
+        if(plantType == EnumPlantType.Plains)
+            return true;
+
+        return false;
+    }
+}
