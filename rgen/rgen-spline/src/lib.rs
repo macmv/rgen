29d82aa8406c5f6d4f2a8e89d9bf956d7155ea0a
Neil Macneale V <neil.macneale.v@gmail.com>
2024-03-04 20:11:35 -0800

    Add cosine interpolation

--- rgen/rgen-spline/src/lib.rs
+++ rgen/rgen-spline/src/lib.rs
@@ -46,9 +46,16 @@ pub trait Interpolation {
 }
 
 pub struct Linear;
-
 impl Interpolation for Linear {
   fn interpolate(t: f64, left: f64, right: f64) -> f64 { left + (right - left) * t }
+}
+
+pub struct Cosine;
+impl Interpolation for Cosine {
+  fn interpolate(t: f64, left: f64, right: f64) -> f64 {
+    let cos_t = (1. - (t * std::f64::consts::PI).cos()) * 0.5;
+    Linear::interpolate(cos_t, left, right)
+  }
 }
 
 impl<T: SplineStorage> Spline<T> {
