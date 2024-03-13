eaaffaa20c28876b7ab01f8df1b85d07e51300eb
Neil Macneale V <neil.macneale.v@gmail.com>
2024-03-12 22:19:45 -0700

    Can spawn in some vanilla decorations and such, but no villages

--- src/main/java/net/macmv/rgen/world/RGenChunkGenerator.java
+++ src/main/java/net/macmv/rgen/world/RGenChunkGenerator.java
@@ -1,24 +1,40 @@
 package net.macmv.rgen.world;
 
 import net.macmv.rgen.rust.RustGenerator;
+import net.minecraft.block.BlockFalling;
 import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEntitySpawner;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
+import net.minecraft.world.gen.ChunkGeneratorOverworld;
 import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.feature.WorldGenDungeons;
+import net.minecraft.world.gen.feature.WorldGenLakes;
+import net.minecraftforge.event.ForgeEventFactory;
+import net.minecraftforge.event.terraingen.PopulateChunkEvent;
+import net.minecraftforge.event.terraingen.TerrainGen;
 
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
-public class RGenChunkGenerator implements IChunkGenerator {
+public class RGenChunkGenerator extends ChunkGeneratorOverworld {
   private final World world;
+  private final Random rand;
+  private final VanillaDecorator vanillaDecorator = new VanillaDecorator();
 
   public RGenChunkGenerator(World world) {
+    super(world, world.getSeed(), false, "");
     this.world = world;
+    this.rand = new Random(world.getSeed());
 
     RustGenerator.init(world.getSeed());
   }
@@ -56,33 +72,73 @@ public class RGenChunkGenerator implements IChunkGenerator {
   }
 
   @Override
-  public void populate(int x, int z) {
-
-  }
-
-  @Override
-  public boolean generateStructures(Chunk chunkIn, int x, int z) {
-    return false;
-  }
-
-  @Override
   public List<Biome.SpawnListEntry> getPossibleCreatures(EnumCreatureType creatureType, BlockPos pos) {
     return Collections.emptyList();
   }
 
-  @Nullable
   @Override
-  public BlockPos getNearestStructurePos(World worldIn, String structureName, BlockPos position, boolean findUnexplored) {
-    return BlockPos.ORIGIN;
-  }
+  public void populate(int x, int z) {
+    long startTime = System.nanoTime();
 
-  @Override
-  public void recreateStructures(Chunk chunkIn, int x, int z) {
+    BlockFalling.fallInstantly = true;
+    int i = x * 16;
+    int j = z * 16;
+    BlockPos blockpos = new BlockPos(i, 0, j);
+    Biome biome = this.world.getBiome(blockpos.add(16, 0, 16));
+    this.rand.setSeed(this.world.getSeed());
+    long k = this.rand.nextLong() / 2L * 2L + 1L;
+    long l = this.rand.nextLong() / 2L * 2L + 1L;
+    this.rand.setSeed((long) x * k + (long) z * l ^ this.world.getSeed());
+    boolean flag = false;
+    ChunkPos chunkpos = new ChunkPos(x, z);
 
-  }
+    ForgeEventFactory.onChunkPopulate(true, this, this.world, this.rand, x, z, flag);
 
-  @Override
-  public boolean isInsideStructure(World worldIn, String structureName, BlockPos pos) {
-    return false;
+    this.vanillaDecorator.decorate(this.world, this.rand, chunkpos);
+
+    if (TerrainGen.populate(this, this.world, this.rand, x, z, flag, PopulateChunkEvent.Populate.EventType.DUNGEON)) {
+      // dungeonChance is 8
+      for (int j2 = 0; j2 < 8; ++j2) {
+        int i3 = this.rand.nextInt(16) + 8;
+        int l3 = this.rand.nextInt(256);
+        int l1 = this.rand.nextInt(16) + 8;
+        (new WorldGenDungeons()).generate(this.world, this.rand, blockpos.add(i3, l3, l1));
+      }
+    }
+
+    // biome.decorate(this.world, this.rand, new BlockPos(i, 0, j));
+
+    if (TerrainGen.populate(this, this.world, this.rand, x, z, flag, PopulateChunkEvent.Populate.EventType.ANIMALS)) {
+      WorldEntitySpawner.performWorldGenSpawning(this.world, biome, i + 8, j + 8, 16, 16, this.rand);
+    }
+
+    // TODO: Move this over to rust.
+    /*
+    blockpos = blockpos.add(8, 0, 8);
+    if (TerrainGen.populate(this, this.world, this.rand, x, z, flag, PopulateChunkEvent.Populate.EventType.ICE)) {
+      for (int k2 = 0; k2 < 16; ++k2) {
+        for (int j3 = 0; j3 < 16; ++j3) {
+          BlockPos blockpos1 = this.world.getPrecipitationHeight(blockpos.add(k2, 0, j3));
+          BlockPos blockpos2 = blockpos1.down();
+
+          if (this.world.canBlockFreezeWater(blockpos2)) {
+            this.world.setBlockState(blockpos2, Blocks.ICE.getDefaultState(), 2);
+          }
+
+          if (this.world.canSnowAt(blockpos1, true)) {
+            this.world.setBlockState(blockpos1, Blocks.SNOW_LAYER.getDefaultState(), 2);
+          }
+        }
+      }
+    }
+     */
+
+    ForgeEventFactory.onChunkPopulate(false, this, this.world, this.rand, x, z, flag);
+
+    BlockFalling.fallInstantly = false;
+
+    long endTime = System.nanoTime();
+    long duration = (endTime - startTime);
+    System.out.println("chunk took " + (float) duration / 1_000_000f + " millis");
   }
 }
---
+++ src/main/java/net/macmv/rgen/world/VanillaDecorator.java
@@ -0,0 +1,40 @@
+package net.macmv.rgen.world;
+
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.MapGenBase;
+import net.minecraft.world.gen.MapGenCaves;
+import net.minecraft.world.gen.MapGenRavine;
+import net.minecraft.world.gen.structure.*;
+
+import java.util.Random;
+
+// This is basically all the private fields from the overworld generator, like mineshafts and
+// villages.
+public class VanillaDecorator {
+  private MapGenBase caveGenerator = new MapGenCaves();
+  private MapGenStronghold strongholdGenerator = new MapGenStronghold();
+  private MapGenVillage villageGenerator = new MapGenVillage();
+  private MapGenMineshaft mineshaftGenerator = new MapGenMineshaft();
+  private MapGenScatteredFeature scatteredFeatureGenerator = new MapGenScatteredFeature();
+  private MapGenBase ravineGenerator = new MapGenRavine();
+  private StructureOceanMonument oceanMonumentGenerator = new StructureOceanMonument();
+
+  public VanillaDecorator() {
+    caveGenerator = net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(caveGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.CAVE);
+    strongholdGenerator = (MapGenStronghold) net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(strongholdGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.STRONGHOLD);
+    villageGenerator = (MapGenVillage) net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(villageGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.VILLAGE);
+    mineshaftGenerator = (MapGenMineshaft) net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(mineshaftGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.MINESHAFT);
+    scatteredFeatureGenerator = (MapGenScatteredFeature) net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(scatteredFeatureGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.SCATTERED_FEATURE);
+    ravineGenerator = net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(ravineGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.RAVINE);
+    oceanMonumentGenerator = (StructureOceanMonument) net.minecraftforge.event.terraingen.TerrainGen.getModdedMapGen(oceanMonumentGenerator, net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.OCEAN_MONUMENT);
+  }
+
+  public void decorate(World world, Random rand, ChunkPos chunk_pos) {
+    this.mineshaftGenerator.generateStructure(world, rand, chunk_pos);
+    this.villageGenerator.generateStructure(world, rand, chunk_pos);
+    this.strongholdGenerator.generateStructure(world, rand, chunk_pos);
+    this.scatteredFeatureGenerator.generateStructure(world, rand, chunk_pos);
+    this.oceanMonumentGenerator.generateStructure(world, rand, chunk_pos);
+  }
+}
