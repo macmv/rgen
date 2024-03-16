88e4c4df3bbc64b2d7540cf8b50ceac590978f63
Neil Macneale V <neil.macneale.v@gmail.com>
2024-03-15 19:28:46 -0700

    Update rgen-placer to consume a seed on init

--- rgen/rgen-placer/benches/noise.rs
+++ rgen/rgen-placer/benches/noise.rs
@@ -7,20 +7,19 @@ use test::Bencher;
 
 #[bench]
 fn bench_perlin(b: &mut Bencher) {
-  let noise: OctavedNoise<PerlinNoise> =
-    OctavedNoise { octaves: 16, freq: 1.0 / 4096.0, ..Default::default() };
+  let noise: OctavedNoise<PerlinNoise, 16> = OctavedNoise::new(1.0 / 4096.0, PerlinNoise::new);
 
   b.iter(|| {
-    test::black_box(noise.generate(0.0, 0.0, 0));
+    test::black_box(noise.generate(0.0, 0.0));
   });
 }
 
 #[bench]
 fn bench_open_simplex(b: &mut Bencher) {
-  let noise: OctavedNoise<OpenSimplexNoise> =
-    OctavedNoise { octaves: 16, freq: 1.0 / 4096.0, ..Default::default() };
+  let noise: OctavedNoise<OpenSimplexNoise, 16> =
+    OctavedNoise::new(1.0 / 4096.0, OpenSimplexNoise::new);
 
   b.iter(|| {
-    test::black_box(noise.generate(0.0, 0.0, 0));
+    test::black_box(noise.generate(0.0, 0.0));
   });
 }
--- rgen/rgen-placer/src/noise/mod.rs
+++ rgen/rgen-placer/src/noise/mod.rs
@@ -7,9 +7,9 @@ pub use open_simplex::OpenSimplexNoise;
 pub use perlin::PerlinNoise;
 
 pub trait NoiseGenerator {
-  fn generate(&self, x: f64, y: f64, seed: u64) -> f64;
+  fn generate(&self, x: f64, y: f64) -> f64;
 }
 
 pub trait NoiseGenerator3D {
-  fn generate_3d(&self, x: f64, y: f64, z: f64, seed: u64) -> f64;
+  fn generate_3d(&self, x: f64, y: f64, z: f64) -> f64;
 }
