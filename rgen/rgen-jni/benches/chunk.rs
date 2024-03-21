74ae0fe59d7aad88e9504b4f37d852bb4541f889
Neil Macneale V <neil.macneale.v@gmail.com>
2024-03-20 18:51:07 -0700

    Add benchmark for generating the base layer of a chunk

--- rgen/rgen-jni/benches/chunk.rs
+++ rgen/rgen-jni/benches/chunk.rs
@@ -4,9 +4,9 @@ extern crate test;
 
 use std::sync::Arc;
 
-use rgen_base::{Biomes, Blocks, ChunkPos};
+use rgen_base::{Biomes, Blocks, Chunk, ChunkPos};
 use rgen_biome::WorldBiomes;
-use rgen_world::CachedWorld;
+use rgen_world::{CachedWorld, Generator, PartialWorld};
 use test::Bencher;
 
 #[bench]
@@ -30,5 +30,28 @@ fn bench_chunk(b: &mut Bencher) {
     }
 
     world.generate(chunk_pos, |_| {});
+  });
+}
+
+#[bench]
+fn bench_base(b: &mut Bencher) {
+  let context = Arc::new(rgen_world::Context {
+    seed:   1 as u64,
+    blocks: Blocks::test_blocks(),
+    biomes: Biomes::test_blocks(),
+  });
+  let generator = Arc::new(WorldBiomes::new(&context.blocks, &context.biomes, context.seed as u64));
+
+  let mut chunk_pos = ChunkPos::new(0, 0);
+
+  b.iter(|| {
+    chunk_pos.x += 1;
+    if chunk_pos.x > 20 {
+      chunk_pos.x = 0;
+      chunk_pos.z += 1;
+    }
+
+    let mut chunk = Chunk::new();
+    generator.generate_base(&context, &mut chunk, chunk_pos);
   });
 }
