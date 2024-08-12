2ae62251f745111ec0043102ba457bdf6ac67ed1
Neil Macneale V <neil.macneale.v@gmail.com>
2024-08-11 17:47:49 -0700

    Update generation to be members of Village

--- rgen/rgen-biome/src/structure/village/mod.rs
+++ rgen/rgen-biome/src/structure/village/mod.rs
@@ -1,5 +1,4 @@
 use rgen_base::{BlockState, Chunk, ChunkPos, Pos};
-use rgen_llama::Structure;
 use rgen_placer::{grid::PointGrid, Random, Rng};
 
 use crate::biome::IdContext;
@@ -53,25 +52,27 @@ struct Village<'a> {
   origin: Pos,
 }
 
+#[derive(Clone, Copy)]
 struct Road {
   start: Pos,
   end:   Pos,
 }
 
+#[derive(Clone, Copy)]
 struct Building {
   pos: Pos,
 }
 
 impl<'a> Village<'a> {
   pub fn new(generator: &'a VillageGenerator, seed: u64, origin: Pos) -> Self {
-    let mut roads = vec![];
+    let mut village = Village { generator, roads: vec![], buildings: vec![], origin };
+
     let mut rng = Rng::new(seed);
-    recursive_road(&mut roads, &mut rng, origin, origin, 0);
+    village.recursive_road(&mut rng, origin, origin, 0);
 
-    let mut buildings = vec![];
-    place_buildings(&mut buildings, &mut rng, &roads);
+    village.place_buildings(&mut rng);
 
-    Village { generator, roads, buildings, origin }
+    village
   }
 
   pub fn generate(&self, chunk: &mut Chunk, chunk_pos: ChunkPos) {
@@ -124,42 +125,49 @@ impl<'a> Village<'a> {
   }
 }
 
-fn recursive_road(roads: &mut Vec<Road>, rng: &mut Rng, origin: Pos, pos: Pos, depth: u32) {
-  if depth > 3 {
-    return;
-  }
-
-  let mut dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
-  rng.shuffle(&mut dirs);
-
-  for (dx, dz) in dirs {
-    if rng.rand_inclusive(0, 2) < 1 {
-      continue;
+// Village generation
+impl<'a> Village<'a> {
+  fn recursive_road(&mut self, rng: &mut Rng, origin: Pos, pos: Pos, depth: u32) {
+    if depth > 3 {
+      return;
     }
 
-    let length = rng.rand_inclusive(8, 32);
+    let mut dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+    rng.shuffle(&mut dirs);
 
-    let new_pos = pos + Pos::new(dx * length, 0, dz * length);
-    if roads.iter().any(|road| {
-      let min_x = road.start.x.min(road.end.x) - 4;
-      let max_x = road.start.x.max(road.end.x) + 4;
-      let min_z = road.start.z.min(road.end.z) - 4;
-      let max_z = road.start.z.max(road.end.z) + 4;
+    for (dx, dz) in dirs {
+      if rng.rand_inclusive(0, 2) < 1 {
+        continue;
+      }
 
-      new_pos.x >= min_x && new_pos.x <= max_x && new_pos.z >= min_z && new_pos.z <= max_z
-    }) {
-      continue;
+      let length = rng.rand_inclusive(8, 32);
+
+      let new_pos = pos + Pos::new(dx * length, 0, dz * length);
+      if self.roads.iter().any(|road| {
+        let min_x = road.start.x.min(road.end.x) - 4;
+        let max_x = road.start.x.max(road.end.x) + 4;
+        let min_z = road.start.z.min(road.end.z) - 4;
+        let max_z = road.start.z.max(road.end.z) + 4;
+
+        new_pos.x >= min_x && new_pos.x <= max_x && new_pos.z >= min_z && new_pos.z <= max_z
+      }) {
+        continue;
+      }
+
+      let road = Road { start: pos, end: new_pos };
+      self.roads.push(road);
+
+      self.recursive_road(rng, origin, new_pos, depth + 1);
     }
-
-    let road = Road { start: pos, end: new_pos };
-    roads.push(road);
-
-    recursive_road(roads, rng, origin, new_pos, depth + 1);
   }
-}
 
-fn place_buildings(buildings: &mut Vec<Building>, rng: &mut Rng, roads: &[Road]) {
-  fn place_buildings_along(buildings: &mut Vec<Building>, _rng: &mut Rng, road: &Road) {
+  fn place_buildings(&mut self, rng: &mut Rng) {
+    for road in self.roads.clone() {
+      self.place_buildings_along(rng, &road);
+    }
+  }
+
+  fn place_buildings_along(&mut self, _rng: &mut Rng, road: &Road) {
     let mut i = 0;
 
     let off_axis = if road.start.x != road.end.x { (0, 1) } else { (1, 0) };
@@ -174,13 +182,9 @@ fn place_buildings(buildings: &mut Vec<Building>, rng: &mut Rng, roads: &[Road])
 
           let pos = Pos::new(x + off_axis.0 * side * 4, 100, z + off_axis.1 * side * 4);
 
-          buildings.push(Building { pos })
+          self.buildings.push(Building { pos })
         }
       }
     }
-  }
-
-  for road in roads {
-    place_buildings_along(buildings, rng, road);
   }
 }
