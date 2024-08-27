f71f71f7a5c1908ab733ab82764d31eefe7cc22d
Jaimie <jaimiegrichards@gmail.com>
2024-08-27 01:06:30 -0700

    more cactus fixes and additons

--- src/main/java/net/macmv/rgen/block/RgenCactus.java
+++ src/main/java/net/macmv/rgen/block/RgenCactus.java
@@ -1,13 +1,48 @@
 package net.macmv.rgen.block;
 
+import net.macmv.rgen.MathUtil;
 import net.minecraft.block.Block;
-import net.minecraft.block.BlockCactus;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
-import static net.minecraft.block.material.Material.*;
+public class RgenCactus extends Block {
+    protected static final AxisAlignedBB AABB_CACTUS = MathUtil.aabb(1, 0, 1, 14, 15, 14);
 
-public class RgenCactus extends BlockCactus {
-    public RgenCactus(){
-        super();
+    public RgenCactus() {
+        super(Material.PLANTS);
+    }
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {return AABB_CACTUS;}
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {return AABB_CACTUS;}
+
+    @SideOnly(Side.CLIENT)
+    public BlockRenderLayer getBlockLayer()
+    {
+        return BlockRenderLayer.CUTOUT;
+    }
+
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
+    {
+        entityIn.attackEntityFrom(DamageSource.CACTUS, 1.0F);
     }
 }
