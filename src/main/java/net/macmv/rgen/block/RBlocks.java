ee357916b5d0034e402fb21111aa6b3a9d75ff42
Jaimie <jaimiegrichards@gmail.com>
2024-08-26 21:59:56 -0700

    cactus now work a bit more

--- src/main/java/net/macmv/rgen/block/RBlocks.java
+++ src/main/java/net/macmv/rgen/block/RBlocks.java
@@ -38,7 +38,7 @@ public final class RBlocks {
 
   public static final Block FLOWER = register("flower", new FlowerBlock());
   public static final Block CACTUS_ARM = register("cactus_arm", new CactusArm());
-  public static final Block RgenCactus = register("rgen_cactus", new RgenCactus());
+  public static final Block RGEN_CACTUS = register("rgen_cactus", new RgenCactus());
 
   private static Block register(String name, Block block) {
     if (block.getCreativeTabToDisplayOn() == null) {
--- src/main/java/net/macmv/rgen/block/RgenCactus.java
+++ src/main/java/net/macmv/rgen/block/RgenCactus.java
@@ -1,10 +1,13 @@
 package net.macmv.rgen.block;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockCactus;
 import net.minecraft.block.material.Material;
 
 import static net.minecraft.block.material.Material.*;
 
 public class RgenCactus extends BlockCactus {
-    public RgenCactus(){}
+    public RgenCactus(){
+        super();
+    }
 }
