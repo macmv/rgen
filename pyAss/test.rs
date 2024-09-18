f3771eecf633088f39c4ca39d978028d56fc6320
Jaimie <jaimiegrichards@gmail.com>
2024-09-18 00:48:21 -0700

    working test example

---
+++ pyAss/test.rs
@@ -0,0 +1,89 @@
+let (GeographicType::Standard, ClimateType::Warm_temperate) = &[
+    b!(6.0, woodland),
+    b!(1.0, birch_woodland),
+    b!(2.0, aspen_wood),
+    b!(5.0, birch_woodland),
+    b!(4.0, cherry_blossom_grove),
+    b!(2.0, volcano_growth),
+];
+
+let (GeographicType::River, ClimateType::Warm_temperate) = &[
+    b!(6.0, woodland_river),
+    b!(1.0, birch_river),
+    b!(2.0, birch_river),
+    b!(5.0, birch_river),
+    b!(4.0, cherry_blossom_river),
+    b!(2.0, valcano_river),
+];
+
+let (GeographicType::Valley, ClimateType::Warm_temperate) = &[
+    b!(6.0, woodland),
+    b!(1.0, birch_woodland),
+    b!(2.0, aspen_wood),
+    b!(5.0, birch_woodland),
+    b!(4.0, cherry_blossom_grove),
+    b!(2.0, volcano_growth),
+];
+
+let (GeographicType::Hills, ClimateType::Warm_temperate) = &[
+    b!(6.0, woodland_hills),
+    b!(1.0, birch_woodland_hills),
+    b!(2.0, aspen_wood),
+    b!(5.0, birch_woodland_hills),
+    b!(4.0, cherry_blossom_wood),
+    b!(2.0, warm_volcano),
+];
+
+let (GeographicType::Mountain, ClimateType::Warm_temperate) = &[
+    b!(6.0, windswept_woodland),
+    b!(1.0, windswept_birch_woodland),
+    b!(2.0, windswept_aspen_wood),
+    b!(5.0, windswept_birch_woodland),
+    b!(4.0, cherry_blossom_wood),
+    b!(2.0, warm_volcano),
+];
+
+let (GeographicType::Standard, ClimateType::Wet_temperate) = &[
+    b!(6.0, cedar_wood),
+    b!(1.0, cedar_wood),
+    b!(2.0, elder_woodland),
+    b!(5.0, elder_woodland),
+    b!(4.0, weeping_birchwood),
+    b!(2.0, fungal_wood),
+];
+
+let (GeographicType::River, ClimateType::Wet_temperate) = &[
+    b!(6.0, cedar_wood),
+    b!(1.0, cedar_wood),
+    b!(2.0, elder_woodland),
+    b!(5.0, elder_woodland),
+    b!(4.0, weeping_birchwood),
+    b!(2.0, fungal_wood),
+];
+
+let (GeographicType::Valley, ClimateType::Wet_temperate) = &[
+    b!(6.0, cedar_wood),
+    b!(1.0, cedar_wood),
+    b!(2.0, elder_woodland),
+    b!(5.0, elder_woodland),
+    b!(4.0, weeping_birchwood),
+    b!(2.0, fungal_wood),
+];
+
+let (GeographicType::Hills, ClimateType::Wet_temperate) = &[
+    b!(6.0, cedar_wood),
+    b!(1.0, cedar_wood),
+    b!(2.0, elder_woodland),
+    b!(5.0, elder_woodland),
+    b!(4.0, weeping_birchwood),
+    b!(2.0, fungal_wood),
+];
+
+let (GeographicType::Mountain, ClimateType::Wet_temperate) = &[
+    b!(6.0, cedar_wood),
+    b!(1.0, cedar_wood),
+    b!(2.0, elder_woodland),
+    b!(5.0, elder_woodland),
+    b!(4.0, weeping_birchwood),
+    b!(2.0, fungal_wood),
+];
