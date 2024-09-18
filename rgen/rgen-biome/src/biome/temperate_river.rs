54809b57f4260f2f73c92140f59445b155a47a2f
Jaimie <jaimiegrichards@gmail.com>
2024-09-18 15:58:29 -0700

    fixez the missing birch river

--- rgen/rgen-biome/src/biome/temperate_river.rs
+++ rgen/rgen-biome/src/biome/temperate_river.rs
@@ -67,61 +67,6 @@ pub fn cherry_blossom_river(ctx: &IdContext, gen: &mut BiomeBuilder) {
   );
 }
 
-pub fn birch_river(ctx: &IdContext, gen: &mut BiomeBuilder) {
-  gen.id = ctx.biomes.birch_forest;
-  gen.color = "#768A6A";
-  gen.set_top_block(ctx.blocks.grass.default_state);
-
-  gen.place("Basalt Pillar", PlacerStage::Tree, placer::RiverSide::new(ctx.blocks));
-
-  gen.place(
-    "birch log",
-    PlacerStage::Tree,
-    placer::LogAndStump {
-      log:            ctx.blocks.log.with_data(2),
-      moss_log:       ctx.blocks.rgen_mossy_stump.with_data(1),
-      ground:         ctx.blocks.grass.default_state,
-      plants:         ctx.blocks.stone.default_state.into(),
-      avg_per_chunk:  1.75,
-      chance_of_moss: 5,
-      is_shrooms:     true,
-      shroom:         ctx.blocks.rgen_polypore.default_state,
-    },
-  );
-  gen.place(
-    "basic birch tree",
-    PlacerStage::Tree,
-    placer::BasicBirch {
-      trunk:            ctx.blocks.log.with_data(2),
-      leaves:           ctx.blocks.leaves.with_data(2),
-      avg_per_chunk:    5.0,
-      is_shrooms:       true,
-      chance_of_shroom: 100.0,
-      shroom:           ctx.blocks.rgen_polypore.with_data(0),
-      ground:           ctx.blocks.grass.default_state,
-    },
-  );
-
-  gen.place(
-    "grass",
-    PlacerStage::Tree,
-    placer::Scatter {
-      attempts:    160,
-      place_above: [ctx.blocks.grass.block, ctx.blocks.rgen_mossy_stump.block].into(),
-      place:       ctx.blocks.tallgrass.with_data(2),
-    },
-  );
-  gen.place(
-    "ferns",
-    PlacerStage::Tree,
-    placer::Scatter {
-      attempts:    160,
-      place_above: [ctx.blocks.grass.block, ctx.blocks.rgen_mossy_stump.block].into(),
-      place:       ctx.blocks.tallgrass.with_data(1),
-    },
-  );
-}
-
 pub fn lavender_river(ctx: &IdContext, gen: &mut BiomeBuilder) {
   gen.id = ctx.biomes.birch_forest;
   gen.color = "#899781";
