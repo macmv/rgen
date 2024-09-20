7e06287357f0158058ab40ed2fddbc7352178698
Neil Macneale V <neil.macneale.v@gmail.com>
2024-09-19 22:43:10 -0700

    Make crevasse above ground level

--- rgen/rgen-placer/src/chunk_placer/crevasse.rs
+++ rgen/rgen-placer/src/chunk_placer/crevasse.rs
@@ -7,11 +7,10 @@ use crate::{
 };
 
 pub struct Crevasse {
-  pub replace: BlockFilter,
+  pub replace:    BlockFilter,
+  pub packed_ice: BlockState,
 
-  pub depth: i32,
-
-  pub ice: BlockState,
+  pub height: i32,
 
   pub noise: OctavedNoise<OpenSimplexNoise, 4>,
 }
@@ -19,16 +18,16 @@ pub struct Crevasse {
 impl Crevasse {
   pub fn new(blocks: &Blocks) -> Self {
     Crevasse {
-      replace: blocks.stone.block.into(),
-      depth:   10,
-      ice:     blocks.ice.default_state,
-      noise:   OctavedNoise::new(0, 1.0 / 32.0),
+      replace:    blocks.packed_ice.block.into(),
+      height:     10,
+      packed_ice: blocks.packed_ice.default_state,
+      noise:      OctavedNoise::new(0, 1.0 / 32.0),
     }
   }
 }
 
 pub static DEPTH: Spline<&'static [(f64, f64)]> =
-  Spline::new(&[(-1.0, 0.0), (-0.1, 0.0), (0.0, 1.0), (0.1, 0.0), (1.0, 0.0)]);
+  Spline::new(&[(0.00, 1.0), (0.48, 1.0), (0.50, 0.0), (0.52, 1.0), (1.00, 1.0)]);
 
 impl ChunkPlacer for Crevasse {
   fn place(
@@ -45,22 +44,19 @@ impl ChunkPlacer for Crevasse {
         }
 
         let pos = Pos::new(x as i32, 255, z as i32) + chunk_pos.min_block_pos();
-        let depth_value = DEPTH.sample::<Cosine>(self.noise.generate(pos.x as f64, pos.z as f64));
-        let target_depth = (depth_value * self.depth as f64) as i32;
+        let depth_value =
+          DEPTH.sample::<Cosine>((self.noise.generate(pos.x as f64, pos.z as f64) + 1.0) / 2.0);
+        let target_depth = (depth_value * self.height as f64) as i32;
 
-        let mut depth = 0;
         for y in (0..256).rev() {
           let rel_pos = rel_pos.with_y(y);
 
           let block = chunk.chunk.get_state(rel_pos);
-          if depth != 0 || self.replace.contains(block) {
-            depth += 1;
-          }
+          if self.replace.contains(block) {
+            for i in 0..target_depth {
+              chunk.chunk.set(rel_pos.with_y(y + i), self.packed_ice);
+            }
 
-          if depth < target_depth {
-            chunk.chunk.set(rel_pos, BlockState::AIR);
-          }
-          if depth >= target_depth {
             break;
           }
         }
