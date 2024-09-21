539eb4ef2d5a244cb1615666a99e8681ebdb4610
Jaimie <jaimiegrichards@gmail.com>
2024-09-20 21:32:24 -0700

    working taller snow edges

--- rgen/rgen-placer/src/placer/ice/better_taller_snow.rs
+++ rgen/rgen-placer/src/placer/ice/better_taller_snow.rs
@@ -7,6 +7,7 @@ use crate::{chunk, Placer, Random, Rng};
 pub struct BetterTallerSnow {
   pub block:        BlockFilter,
   pub snow:         BlockState,
+  pub debug:        BlockState,
   pub avg_in_chunk: f64,
 }
 
@@ -15,7 +16,8 @@ impl BetterTallerSnow {
     BetterTallerSnow {
       block:        [blocks.snow_layer.block].into(),
       snow:         blocks.snow_layer.default_state,
-      avg_in_chunk: 0.1,
+      debug:        blocks.concrete.with_data(5),
+      avg_in_chunk: 1.0,
     }
   }
 }
@@ -32,27 +34,31 @@ impl Placer for BetterTallerSnow {
     let chunk_pos = pos.chunk();
     for z in 0..16 {
       for x in 0..16 {
-        let pos = chunk_pos.min_block_pos() + Pos::new(x, 0, z);
-        if world.get(pos) == self.snow {
-          'outer: for rel_x in -1..=1_i32 {
-            for rel_z in -1..=1_i32 {
-              if !(rel_x == 0 && rel_z == 0) || !(rel_x.abs() == 1 && rel_z.abs() == 1) {
-                let block_check = world.get(pos + Pos::new(rel_x, 0, rel_z));
-                if self.block.contains(block_check) && block_check.block != Block::AIR {
-                  let mut height = world.get(pos).state;
-                  height += rng.rand_inclusive(4, 7) as u8;
-                  let mut level = 0;
-                  while height > 7 {
-                    height -= 7;
-                    world.set(pos + Pos::new(0, level, 0), self.snow.with_data(7));
-                    level += 1;
+        for y in (0..256).rev() {
+          let pos = chunk_pos.min_block_pos() + Pos::new(x, y, z);
+          if self.block.contains(world.get(pos)) {
+            'outer: for rel_x in -1..=1_i32 {
+              for rel_z in -1..=1_i32 {
+                if !(rel_x == 0 && rel_z == 0) || !(rel_x.abs() == 1 && rel_z.abs() == 1) {
+                  let block_check = world.get(pos + Pos::new(rel_x, 0, rel_z));
+                  if !self.block.contains(block_check) && block_check.block != Block::AIR {
+                    let mut height = world.get(pos).state;
+                    height += 3; //rng.rand_inclusive(3, 5) as u8;
+                    println!("height: {}", height);
+                    let mut level = 0;
+                    while height > 7 {
+                      height -= 7;
+                      world.set(pos + Pos::new(0, level, 0), self.snow.with_data(7));
+                      level += 1;
+                    }
+                    world.set(pos + Pos::new(0, level, 0), self.snow.with_data(height));
+                    //rld.set(pos + Pos::new(0, 15, 0), self.debug);
+                    break 'outer;
                   }
-                  world.set(pos + Pos::new(0, level, 0), self.snow.with_data(7));
-
-                  break 'outer;
                 }
               }
             }
+            break;
           }
         }
       }
