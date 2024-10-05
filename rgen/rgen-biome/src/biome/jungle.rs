70dbdae5afde42df614a2c7eb852ddf40e1a1da2
Neil Macneale V <neil.macneale.v@gmail.com>
2024-10-04 22:39:31 -0700

    Add bushes

--- rgen/rgen-biome/src/biome/jungle.rs
+++ rgen/rgen-biome/src/biome/jungle.rs
@@ -11,4 +11,16 @@ pub fn deep_jungle(ctx: &IdContext, gen: &mut BiomeBuilder) {
   gen.add_layer(ctx.blocks.dirt.default_state, 5, 8);
 
   gen.place("Large Jungle Tree", PlacerStage::Tree, placer::JungleTree::new(&ctx.blocks));
+
+  gen.place(
+    "Jungle bushes",
+    PlacerStage::Tree,
+    placer::BushClumps {
+      place_above:   [ctx.blocks.grass.block].into(),
+      log:           ctx.blocks.log.with_data(3),
+      leaves:        ctx.blocks.leaves.with_data(3),
+      avg_per_chunk: 6.0,
+      radius:        3..=5,
+    },
+  );
 }
