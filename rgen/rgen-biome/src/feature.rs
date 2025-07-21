e23b1964078f418f2b80c8ecd307d32981890c98
Jaimie <jaimiegrichards@gmail.com>
2025-07-20 23:23:46 -0700

    final touches

--- rgen/rgen-biome/src/feature.rs
+++ rgen/rgen-biome/src/feature.rs
@@ -19,11 +19,12 @@ pub const GEOGRAPHIC_TYPE_OVERRIDE: Option<GeographicType> = Some(GeographicType
 
 /// Overrides the climate type of the world.
 pub const CLIMATE_TYPE_OVERRIDE: Option<ClimateType> = None;
+//Some(ClimateType::Tropical);
 
 /// Override the entire world to be a single biome (the blank biome).
 /// In table.rs To change the biome, modify the biome_categories macro, and
 /// change the blank biome composition
-pub const BIOME_OVERRIDE: bool = true;
+pub const BIOME_OVERRIDE: bool = false;
 
 /// Replaces everything with a superflat world.
 pub const SUPERFLAT: bool = false;
